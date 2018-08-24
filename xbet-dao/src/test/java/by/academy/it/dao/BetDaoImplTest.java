c1f82e7720d2aaa2b08ae93a6f81e338ebfadcf4
Skwazer <Skwazer@mail.ru>
2018-08-24 21:56:01 +0300

    Updated BetDaoImplTest class using the Mockito framework.

--- xbet-dao/src/test/java/by/academy/it/dao/BetDaoImplTest.java
+++ xbet-dao/src/test/java/by/academy/it/dao/BetDaoImplTest.java
@@ -1,36 +1,86 @@
 package by.academy.it.dao;
 
-import by.academy.it.dao.factory.DaoFactory;
 import by.academy.it.entity.Bet;
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertFalse;
+import java.util.Collections;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
 
 public class BetDaoImplTest {
 
-    BetDao betDao = DaoFactory.getInstance().getBetDao();
+    private static BetDao betDao;
+    private static Bet bet;
+
+    @Before
+    public void setUp() {
+        betDao = mock(BetDao.class);
+        bet = mock(Bet.class);
+    }
 
     @Test
     public void create() throws Exception {
-        Bet bet = new Bet();
-        bet.setUser_id(1);
-        bet.setMatch_id(1);
-        bet.setBetResult("1X");
-        bet.setBet(1.31);
-        bet.setMoney(100500d);
-        bet.setStatus("test_status");
         betDao.create(bet);
-        assertFalse(betDao.findByUserId(1, 0).isEmpty());
+        verify(betDao).create(bet);
+    }
+
+    @Test
+    public void getById() throws Exception {
+        when(betDao.getById(1)).thenReturn(bet);
+        assertEquals(betDao.getById(1), bet);
+        verify(betDao).getById(1);
     }
 
     @Test
     public void findByUserId() throws Exception {
-        assertFalse(betDao.findByUserId(2, 0).isEmpty());
+        assertEquals(betDao.findByUserId(1, 1), Collections.emptyList());
+        verify(betDao).findByUserId(1, 1);
+    }
+
+    @Test
+    public void getAmountOfUserBets() throws Exception {
+        when(betDao.getAmountOfUserBets(1)).thenReturn(1);
+        assertEquals(betDao.getAmountOfUserBets(1), 1);
+        verify(betDao).getAmountOfUserBets(1);
+    }
+
+    @Test
+    public void delete() throws Exception {
+        betDao.delete(1);
+        verify(betDao).delete(1);
+    }
+
+    @Test
+    public void update() throws Exception {
+        betDao.update(bet);
+        verify(betDao).update(bet);
     }
 
     @Test
     public void findByMatchId() throws Exception {
-        assertFalse(betDao.findByMatchId(9).isEmpty());
+        assertEquals(betDao.findByMatchId(1), Collections.emptyList());
+        verify(betDao).findByMatchId(1);
+    }
+
+    @Test
+    public void updateStatus() throws Exception {
+        betDao.updateStatus(bet);
+        verify(betDao).updateStatus(bet);
+    }
+
+    @Test
+    public void findAll() throws Exception {
+        assertEquals(betDao.findAll(1), Collections.emptyList());
+        verify(betDao).findAll(1);
+    }
+
+    @Test
+    public void getAmountOfAllBets() throws Exception {
+        when(betDao.getAmountOfAllBets()).thenReturn(10);
+        assertEquals(betDao.getAmountOfAllBets(), 10);
     }
 
 }
--- xbet-dao/src/test/java/by/academy/it/dao/ResultDaoImplTest.java
+++
@@ -1,5 +0,0 @@
-package by.academy.it.dao;
-
-public class ResultDaoImplTest {
-
-}
